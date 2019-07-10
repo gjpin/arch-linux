a85028ba456e058fa314d07666d198c8ca34c7ae
exah-io <3874515+exah-io@users.noreply.github.com>
2019-07-10 21:50:24 +0100

    systemdb-boot - use UUID

--- 1_install_arch_vm.sh
+++ 1_install_arch_vm.sh
@@ -27,7 +27,7 @@ echo "Creating EFI filesystem"
 yes | mkfs.fat -F32 /dev/sda1
 
 echo "Encrypting / partition"
-printf "%s" "$encryption_passphrase" | cryptsetup -c aes-xts-plain64 -h sha512 -s 512 --use-random --type luks2 --label CRYPTO luksFormat /dev/sda2
+printf "%s" "$encryption_passphrase" | cryptsetup -c aes-xts-plain64 -h sha512 -s 512 --use-random --type luks2 luksFormat /dev/sda2
 printf "%s" "$encryption_passphrase" | cryptsetup luksOpen /dev/sda2 cryptoVols
 
 echo "Setting up LVM"
@@ -98,6 +98,7 @@ timeout 0
 editor 0
 END
 
+PART_ID=$(blkid -o value -s UUID /dev/sda2)
 mkdir -p /boot/loader/entries/
 touch /boot/loader/entries/arch.conf
 tee -a /boot/loader/entries/arch.conf << END
@@ -105,7 +106,7 @@ title ArchLinux
 linux /vmlinuz-linux
 initrd /initramfs-linux.img
 initrd /intel-ucode.img
-options cryptdevice=LABEL=CRYPTO:cryptoVols root=/dev/mapper/Arch-root resume=/dev/mapper/Arch-swap quiet rw
+options cryptdevice=UUID=$PART_ID:cryptoVols root=/dev/mapper/Arch-root resume=/dev/mapper/Arch-swap quiet rw
 END
 
 mkdir -p /etc/pacman.d/hooks/
