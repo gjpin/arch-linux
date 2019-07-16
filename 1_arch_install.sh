d8a3df6abbd1a9f52989ba240997daf15e4ba1bb
exah-io <3874515+exah-io@users.noreply.github.com>
2019-07-16 21:53:45 +0100

    fix backlight in waybar + reflector improvement

--- 1_arch_install.sh
+++ 1_arch_install.sh
@@ -134,7 +134,7 @@ Target = pacman-mirrorlist
 Description = Updating pacman-mirrorlist with reflector and removing pacnew...
 When = PostTransaction
 Depends = reflector
-Exec = /bin/sh -c "reflector --latest 200 --age 12 --protocol https --sort rate --save /etc/pacman.d/mirrorlist; rm -f /etc/pacman.d/mirrorlist.pacnew"
+Exec = /bin/sh -c "reflector --latest 200 --age 12 --protocol https --sort rate --save /etc/pacman.d/mirrorlist"
 END
 
 echo "Enabling periodic TRIM"
