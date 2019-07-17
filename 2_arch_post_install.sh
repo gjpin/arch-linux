37c0d4a7e2302915f4ab7065980f37306f92810f
exah-io <3874515+exah-io@users.noreply.github.com>
2019-07-17 20:20:10 +0100

    Added nnn to list of packages

--- 2_arch_post_install.sh
+++ 2_arch_post_install.sh
@@ -63,7 +63,7 @@ sudo mkdir -p /usr/share/fonts/TTF/
 sudo wget -P /usr/share/fonts/TTF/ https://raw.githubusercontent.com/Templarian/MaterialDesign-Webfont/master/fonts/materialdesignicons-webfont.ttf
 
 echo "Installing sway and additional packages"
-yes | sudo pacman -S sway swaylock swayidle waybar pulseaudio pavucontrol rofi slurp grim thunar mousepad
+yes | sudo pacman -S sway swaylock swayidle waybar pulseaudio pavucontrol rofi slurp grim thunar mousepad nnn
 mkdir -p ~/.config/sway
 wget -P ~/.config/sway/ https://raw.githubusercontent.com/exah-io/minimal-arch-linux/master/configs/sway/config
 mkdir -p ~/Pictures/screenshots
