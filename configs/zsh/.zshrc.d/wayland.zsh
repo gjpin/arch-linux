c72615fa8c840267ede3d932fe6bfc5b2a048f1b
exah-io <3874515+exah-io@users.noreply.github.com>
2019-07-20 14:40:19 +0100

    disabled QT wayland

--- configs/zsh/.zshrc.d/wayland.zsh
+++ configs/zsh/.zshrc.d/wayland.zsh
@@ -2,6 +2,6 @@
 # export GDK_BACKEND=wayland
 
 # Qt5
-export QT_QPA_PLATFORM=wayland-egl
+# export QT_QPA_PLATFORM=wayland-egl
 export QT_AUTO_SCREEN_SCALE_FACTOR=1
 export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
