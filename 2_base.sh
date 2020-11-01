aa13bb769bfa0c361b3f0b06e7a20306e1bb9308
exah-io <3874515+exah-io@users.noreply.github.com>
2020-11-01 15:44:14 +0000

    Update 2_base.sh

--- 2_base.sh
+++ 2_base.sh
@@ -91,6 +91,7 @@ mkdir -p ~/Pictures/wallpapers
 wget -P ~/Pictures/ https://raw.githubusercontent.com/exah-io/minimal-arch-linux/master/images/arch.png
 wget -P ~/Pictures/wallpapers/ https://raw.githubusercontent.com/exah-io/minimal-arch-linux/master/images/wallpapers/tony-liao-8RQi94ZHovg-unsplash.jpg
 wget -P ~/Pictures/wallpapers/ https://raw.githubusercontent.com/exah-io/minimal-arch-linux/master/images/wallpapers/dan-aragon-n20DUSVsUk8-unsplash.jpg
+wget -P ~/Pictures/wallpapers/ https://raw.githubusercontent.com/exah-io/minimal-arch-linux/master/images/wallpapers/robinwall02-notext.png
 
 echo "Ricing bash"
 touch ~/.bashrc
