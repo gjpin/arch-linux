b2d09d5eba3a559714972bd3ffbd228c51150f67
exah-io <3874515+exah-io@users.noreply.github.com>
2020-11-30 17:54:48 +0000

    i3: better fonts, remove close button from gtk apps

--- 2_i3_test.sh
+++ 2_i3_test.sh
@@ -97,6 +97,142 @@ xdg-mime default nautilus.desktop inode/directory
 echo "Creating screenshots folder"
 mkdir -p ~/Pictures/screenshots
 
+# Source: https://aswinmohan.me/posts/better-fonts-on-linux/
+echo "Improving font rendering"
+sudo touch /etc/fonts/local.conf
+sudo tee -a /etc/fonts/local.conf << EOF
+<?xml version='1.0'?>
+<!DOCTYPE fontconfig SYSTEM 'fonts.dtd'>
+<fontconfig>
+
+<match target="font">
+  <edit name="autohint" mode="assign">
+    <bool>true</bool>
+  </edit>
+  <edit name="hinting" mode="assign">
+    <bool>true</bool>
+  </edit>
+  <edit mode="assign" name="hintstyle">
+    <const>hintslight</const>
+  </edit>
+  <edit mode="assign" name="lcdfilter">
+   <const>lcddefault</const>
+ </edit>
+</match>
+
+
+<!-- Default sans-serif font -->
+ <match target="pattern">
+   <test qual="any" name="family"><string>-apple-system</string></test>
+   <!--<test qual="any" name="lang"><string>ja</string></test>-->
+   <edit name="family" mode="prepend" binding="same"><string>Roboto</string>  </edit>
+ </match>
+
+ <match target="pattern">
+   <test qual="any" name="family"><string>Helvetica Neue</string></test>
+   <!--<test qual="any" name="lang"><string>ja</string></test>-->
+   <edit name="family" mode="prepend" binding="same"><string>Roboto</string>  </edit>
+ </match>
+
+ <match target="pattern">
+   <test qual="any" name="family"><string>Helvetica</string></test>
+   <!--<test qual="any" name="lang"><string>ja</string></test>-->
+   <edit name="family" mode="prepend" binding="same"><string>Roboto</string>  </edit>
+ </match>
+
+ <match target="pattern">
+   <test qual="any" name="family"><string>arial</string></test>
+   <!--<test qual="any" name="lang"><string>ja</string></test>-->
+   <edit name="family" mode="prepend" binding="same"><string>Roboto</string>  </edit>
+ </match>
+
+ <match target="pattern">
+   <test qual="any" name="family"><string>sans-serif</string></test>
+   <!--<test qual="any" name="lang"><string>ja</string></test>-->
+   <edit name="family" mode="prepend" binding="same"><string>Roboto</string>  </edit>
+ </match>
+ 
+<!-- Default serif fonts -->
+ <match target="pattern">
+   <test qual="any" name="family"><string>serif</string></test>
+   <edit name="family" mode="prepend" binding="same"><string>Noto Serif</string>  </edit>
+   <edit name="family" mode="prepend" binding="same"><string>Noto Color Emoji</string>  </edit>
+   <edit name="family" mode="append" binding="same"><string>IPAPMincho</string>  </edit>
+   <edit name="family" mode="append" binding="same"><string>HanaMinA</string>  </edit>
+   <edit name="family" mode="prepend" binding="same"><string>Roboto</string>  </edit>
+ </match>
+
+<!-- Default monospace fonts -->
+ <match target="pattern">
+   <test qual="any" name="family"><string>SFMono-Regular</string></test>
+   <edit name="family" mode="prepend" binding="same"><string>Roboto Mono</string></edit>
+   <edit name="family" mode="prepend" binding="same"><string>Space Mono</string></edit>
+   <edit name="family" mode="append" binding="same"><string>Inconsolatazi4</string></edit>
+   <edit name="family" mode="append" binding="same"><string>IPAGothic</string></edit>
+ </match>
+
+ <match target="pattern">
+   <test qual="any" name="family"><string>Menlo</string></test>
+   <edit name="family" mode="prepend" binding="same"><string>Roboto Mono</string></edit>
+   <edit name="family" mode="prepend" binding="same"><string>Space Mono</string></edit>
+   <edit name="family" mode="append" binding="same"><string>Inconsolatazi4</string></edit>
+   <edit name="family" mode="append" binding="same"><string>IPAGothic</string></edit>
+ </match>
+
+ <match target="pattern">
+   <test qual="any" name="family"><string>monospace</string></test>
+   <edit name="family" mode="prepend" binding="same"><string>Roboto Mono</string></edit>
+   <edit name="family" mode="prepend" binding="same"><string>Space Mono</string></edit>
+   <edit name="family" mode="append" binding="same"><string>Inconsolatazi4</string></edit>
+   <edit name="family" mode="append" binding="same"><string>IPAGothic</string></edit>
+ </match>
+
+<!-- Fallback fonts preference order -->
+ <alias>
+  <family>sans-serif</family>
+  <prefer>
+   <family>Noto Sans</family>
+   <family>Noto Color Emoji</family>
+   <family>Noto Emoji</family>
+   <family>Open Sans</family>
+   <family>Droid Sans</family>
+   <family>Ubuntu</family>
+   <family>Roboto</family>
+   <family>NotoSansCJK</family>
+   <family>Source Han Sans JP</family>
+   <family>IPAPGothic</family>
+   <family>VL PGothic</family>
+   <family>Koruri</family>
+  </prefer>
+ </alias>
+ <alias>
+  <family>serif</family>
+  <prefer>
+   <family>Noto Serif</family>
+   <family>Noto Color Emoji</family>
+   <family>Noto Emoji</family>
+   <family>Droid Serif</family>
+   <family>Roboto Slab</family>
+   <family>IPAPMincho</family>
+  </prefer>
+ </alias>
+ <alias>
+  <family>monospace</family>
+  <prefer>
+   <family>Noto Sans Mono</family>
+   <family>Noto Color Emoji</family>
+   <family>Noto Emoji</family>
+   <family>Inconsolatazi4</family>
+   <family>Ubuntu Mono</family>
+   <family>Droid Sans Mono</family>
+   <family>Roboto Mono</family>
+   <family>IPAGothic</family>
+  </prefer>
+ </alias>
+
+</fontconfig>
+EOF
+
 echo "Adding Firefox theme"
 git clone git@github.com:vinceliuice/Orchis-theme.git
 mv ~/Orchis-theme/src/firefox/chrome ~/.var/app/org.mozilla.firefox/.mozilla/firefox/*-release
