4a1d98e1a4b6667673b6817141a8ace2de91b28f
exah-io <3874515+exah-io@users.noreply.github.com>
2021-01-29 17:17:41 +0000

    wip: btrfs support

--- 1_btrfs.sh
+++ 1_btrfs.sh
@@ -31,41 +31,40 @@ echo "Syncing packages database"
 pacman -Sy --noconfirm
 
 echo "Wiping drive"
-sgdisk --zap-all /dev/nvme0n1
+sgdisk --zap-all /dev/vda
 
-echo "Creating partition tables"
-printf "n\n1\n4096\n+512M\nef00\nw\ny\n" | gdisk /dev/nvme0n1
-printf "n\n2\n\n\n8e00\nw\ny\n" | gdisk /dev/nvme0n1
+echo "Creating partitions"
+printf "n\n1\n4096\n+512M\nef00\nw\ny\n" | gdisk /dev/vda
+printf "n\n2\n\n\n8e00\nw\ny\n" | gdisk /dev/vda
 
 echo "Setting up cryptographic volume"
-printf "%s" "$encryption_passphrase" | cryptsetup -h sha512 -s 512 --use-random --type luks2 luksFormat /dev/nvme0n1p2
-printf "%s" "$encryption_passphrase" | cryptsetup luksOpen /dev/nvme0n1p2 cryptlvm
+printf "%s" "$encryption_passphrase" | cryptsetup -h sha512 -s 512 --use-random --type luks2 luksFormat /dev/vda2
+printf "%s" "$encryption_passphrase" | cryptsetup luksOpen /dev/vda2 cryptroot
 
-echo "Creating physical volume"
-pvcreate /dev/mapper/cryptlvm
+echo "Formatting the partitions"
+mkfs.fat -F32 -n LINUXEFI /dev/vda1
+mkfs.btrfs -L Arch /dev/mapper/cryptroot
 
-echo "Creating volume volume"
-vgcreate vg0 /dev/mapper/cryptlvm
+echo "Setting up BTRFS"
+mount -o compress=zstd,noatime /dev/mapper/cryptroot /mnt
+btrfs subvol create /mnt/@
+btrfs subvol create /mnt/@home
+btrfs subvol create /mnt/@swap
 
-echo "Creating logical volumes"
-lvcreate -L +"$swap_size"GB vg0 -n swap
-lvcreate -l +100%FREE vg0 -n root
+mkdir /mnt/snapshots
+btrfs subvol create /mnt/snapshots/@
+btrfs subvol create /mnt/snapshots/@home
 
-echo "Setting up / partition"
-yes | mkfs.ext4 /dev/vg0/root
-mount /dev/vg0/root /mnt
-
-echo "Setting up /boot partition"
-yes | mkfs.fat -F32 /dev/nvme0n1p1
-mkdir /mnt/boot
-mount /dev/nvme0n1p1 /mnt/boot
-
-echo "Setting up swap"
-yes | mkswap /dev/vg0/swap
-swapon /dev/vg0/swap
+umount /mnt
+mkdir -p /mnt/{boot,home,.snapshots}
+mount -o compress=zstd,noatime,subvol=@ /dev/mapper/cryptroot /mnt
+mount -o compress=zstd,noatime,subvol=@home /dev/mapper/cryptroot /mnt/home
+mount -o compress=zstd,noatime,subvol=/snapshots/@ /dev/mapper/cryptroot /mnt/.snapshots/root
+mount -o compress=zstd,noatime,subvol=/snapshots/@home /dev/mapper/cryptroot /mnt/.snapshots/home
+mount /dev/vda1 /mnt/boot
 
 echo "Installing Arch Linux"
-yes '' | pacstrap /mnt base base-devel efibootmgr linux linux-headers linux-lts linux-lts-headers linux-firmware lvm2 device-mapper dosfstools e2fsprogs $cpu_microcode cryptsetup networkmanager wget man-db man-pages nano diffutils flatpak lm_sensors
+yes '' | pacstrap /mnt base base-devel linux linux-headers linux-lts linux-lts-headers linux-firmware efibootmgr btrfs-progs dosfstools e2fsprogs lvm2 device-mapper $cpu_microcode cryptsetup networkmanager wget man-db man-pages nano diffutils flatpak lm_sensors
 
 echo "Generating fstab"
 genfstab -U /mnt >> /mnt/etc/fstab
@@ -96,8 +95,8 @@ useradd -m -G wheel,video -s /bin/bash $username
 echo -en "$user_password\n$user_password" | passwd $username
 
 echo "Generating initramfs"
-sed -i 's/^HOOKS.*/HOOKS=(base systemd autodetect keyboard sd-vconsole modconf block sd-encrypt sd-lvm2 filesystems fsck)/' /etc/mkinitcpio.conf
-sed -i 's/^MODULES.*/MODULES=(ext4 $initramfs_modules)/' /etc/mkinitcpio.conf
+sed -i 's/^HOOKS.*/HOOKS=(base systemd autodetect keyboard sd-vconsole modconf block sd-encrypt fsck)/' /etc/mkinitcpio.conf
+sed -i 's/^MODULES.*/MODULES=(btrfs $initramfs_modules)/' /etc/mkinitcpio.conf
 sed -i 's/#COMPRESSION="lz4"/COMPRESSION="lz4"/g' /etc/mkinitcpio.conf
 mkinitcpio -P
 
@@ -107,8 +106,7 @@ bootctl --path=/boot install
 mkdir -p /boot/loader/
 tee -a /boot/loader/loader.conf << END
 default arch.conf
-timeout 2
-editor 0
+timeout 3
 END
 
 mkdir -p /boot/loader/entries/
@@ -118,7 +116,7 @@ title Arch Linux
 linux /vmlinuz-linux
 initrd /$cpu_microcode.img
 initrd /initramfs-linux.img
-options rd.luks.name=$(blkid -s UUID -o value /dev/nvme0n1p2)=cryptlvm root=/dev/vg0/root resume=/dev/vg0/swap rd.luks.options=discard$kernel_options nmi_watchdog=0 quiet rw
+options rd.luks.name=$(blkid -s UUID -o value /dev/vda2)=cryptroot root=/dev/mapper/cryptroot rootflags=subvol=@ rd.luks.options=discard$kernel_options nmi_watchdog=0 quiet rw
 END
 
 touch /boot/loader/entries/arch-lts.conf
@@ -127,7 +125,7 @@ title Arch Linux LTS
 linux /vmlinuz-linux-lts
 initrd /$cpu_microcode.img
 initrd /initramfs-linux-lts.img
-options rd.luks.name=$(blkid -s UUID -o value /dev/nvme0n1p2)=cryptlvm root=/dev/vg0/root resume=/dev/vg0/swap rd.luks.options=discard$kernel_options nmi_watchdog=0 quiet rw
+options rd.luks.name=$(blkid -s UUID -o value /dev/vda2)=cryptroot root=/dev/mapper/cryptroot rootflags=subvol=@ rd.luks.options=discard$kernel_options nmi_watchdog=0 quiet rw
 END
 
 echo "Setting up Pacman hook for automatic systemd-boot updates"
@@ -143,6 +141,28 @@ Target = systemd
 Description = Updating systemd-boot
 When = PostTransaction
 Exec = /usr/bin/bootctl update
+END
+
+echo "Setting up swap"
+echo "Mounting swapfile subvolume"
+mkdir /swapspace
+mount -o noatime,subvol=@swap /dev/mapper/cryptroot /swapspace
+
+echo "Creating swapfile"
+truncate -s 0 /swapspace/swapfile
+chattr +C /swapspace/swapfile
+btrfs property set /swapspace/swapfile compression none
+fallocate -l "$swap_size"G /swapspace/swapfile
+mkswap /swapspace/swapfile
+chmod 600 /swapspace/swapfile
+
+echo "Activating swapfile"
+swapon /swapspace/swapfile
+
+echo "Adding swap entry to fstab"
+tee -a /etc/crypttab << END
+/dev/mapper/cryptroot /swapspace btrfs rw,noatime,space_cachesubvol=@swap 0 0
+/swapspace/swapfile none swap defaults,discard 0 0
 END
 
 echo "Setting swappiness to 20"
