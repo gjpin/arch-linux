21de2fd1b43cbce7309d0313e2091328bc3fb35b
exah-io <3874515+exah-io@users.noreply.github.com>
2021-02-01 14:42:23 +0000

    base: more granular syncthing firewall rules

--- 2_base.sh
+++ 2_base.sh
@@ -176,7 +176,8 @@ echo "Install syncthing with autostart on boot"
 sudo pacman -S --noconfirm syncthing
 sudo systemctl enable syncthing@$username.service
 sudo systemctl start syncthing@$username.service
-sudo ufw allow syncthing
+sudo ufw allow from 192.168.1.0/24 to any port 22000 proto tcp
+sudo ufw allow from 192.168.1.0/24 to any port 21027 proto udp
 
 echo "Installing pipewire multimedia framework"
 sudo pacman -S --noconfirm pipewire libpipewire02
