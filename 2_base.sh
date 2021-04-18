a777c14740d657263e0d6dc299877866e78ff33a
exah-io <3874515+exah-io@users.noreply.github.com>
2021-04-18 13:23:13 +0100

    chrome flatpak: remove password-store=basic
    not required anymore

--- 2_base.sh
+++ 2_base.sh
@@ -82,7 +82,6 @@ tee -a ~/.var/app/com.google.Chrome/config/chrome-flags.conf << EOF
 --enable-zero-copy
 --enable-accelerated-video-decode
 --use-vulkan
---password-store=basic
 EOF
 
 echo "Installing Chromium Flatpak with GPU acceleration"
