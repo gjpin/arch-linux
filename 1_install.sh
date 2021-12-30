822bc4be0ac02916621a7c4fd99a943d8296f3a0
gjpin <3874515+gjpin@users.noreply.github.com>
2021-12-30 13:24:51 +0000

    add apparmor

--- 1_install.sh
+++ 1_install.sh
@@ -24,84 +24,86 @@ then
  initramfs_modules="intel_agp i915"
 fi
 
-echo "Updating system clock"
+# Update system clock
 timedatectl set-ntp true
 
-echo "Syncing packages database"
+# Sync packages database
 pacman -Sy --noconfirm
 
-echo "Wiping drive"
+# Wipe drive
 sgdisk --zap-all /dev/nvme0n1
 
-echo "Creating partition tables"
+# Create partition tables
 printf "n\n1\n4096\n+512M\nef00\nw\ny\n" | gdisk /dev/nvme0n1
 printf "n\n2\n\n\n8e00\nw\ny\n" | gdisk /dev/nvme0n1
 
-echo "Setting up cryptographic volume"
+# Setup cryptographic volume
 mkdir -p -m0700 /run/cryptsetup
 echo "$encryption_passphrase" | cryptsetup -q --align-payload=8192 -h sha512 -s 512 --use-random --type luks2 -c aes-xts-plain64 luksFormat /dev/nvme0n1p2
 echo "$encryption_passphrase" | cryptsetup luksOpen /dev/nvme0n1p2 cryptlvm
 
-echo "Creating physical volume"
+# Create physical volume
 pvcreate /dev/mapper/cryptlvm
 
-echo "Creating volume volume"
+# Create volume
 vgcreate vg0 /dev/mapper/cryptlvm
 
-echo "Creating logical volumes"
+# Create logical volumes
 lvcreate -L +"$swap_size"GB vg0 -n swap
 lvcreate -l +100%FREE vg0 -n root
 
-echo "Setting up / partition"
+# Setup / partition
 yes | mkfs.ext4 /dev/vg0/root
 mount /dev/vg0/root /mnt
 
-echo "Setting up /boot partition"
+# Setup /boot partition
 yes | mkfs.fat -F32 /dev/nvme0n1p1
 mkdir /mnt/boot
 mount /dev/nvme0n1p1 /mnt/boot
 
-echo "Setting up swap"
+# Setup swap
 yes | mkswap /dev/vg0/swap
 swapon /dev/vg0/swap
 
-echo "Installing Arch Linux"
-yes '' | pacstrap /mnt base base-devel efibootmgr linux linux-headers linux-lts linux-lts-headers linux-firmware lvm2 device-mapper dosfstools e2fsprogs $cpu_microcode cryptsetup networkmanager wget man-db man-pages nano diffutils flatpak lm_sensors
+# Install Arch Linux
+yes '' | pacstrap /mnt base base-devel efibootmgr linux linux-headers linux-lts linux-lts-headers \
+linux-firmware lvm2 device-mapper dosfstools e2fsprogs $cpu_microcode cryptsetup networkmanager\
+wget man-db man-pages nano diffutils flatpak lm_sensors apparmor
 
-echo "Generating fstab"
+# Generate fstab
 genfstab -U /mnt >> /mnt/etc/fstab
 
-echo "Configuring new system"
+# Configure system
 arch-chroot /mnt /bin/bash << EOF
-echo "Setting system clock"
+# Set system clock
 timedatectl set-ntp true
 timedatectl set-timezone $continent_city
 hwclock --systohc --localtime
 
-echo "Setting locales"
+# Set locales
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 locale-gen
 
-echo "Adding persistent keymap"
+# Add persistent keymap
 echo "KEYMAP=us" > /etc/vconsole.conf
 
-echo "Setting hostname"
+# Set hostname
 echo $hostname > /etc/hostname
 
-echo "Setting root password"
+# Set root password
 echo -en "$root_password\n$root_password" | passwd
 
-echo "Creating new user"
+# Create new user
 useradd -m -G wheel,video -s /bin/bash $username
 echo -en "$user_password\n$user_password" | passwd $username
 
-echo "Generating initramfs"
+# Generate initramfs
 sed -i 's/^HOOKS.*/HOOKS=(base systemd autodetect keyboard sd-vconsole modconf block sd-encrypt lvm2 filesystems fsck)/' /etc/mkinitcpio.conf
 sed -i 's/^MODULES.*/MODULES=(ext4 $initramfs_modules)/' /etc/mkinitcpio.conf
 mkinitcpio -P
 
-echo "Setting up systemd-boot"
+# Setup systemd-boot
 bootctl --path=/boot install
 
 mkdir -p /boot/loader/
@@ -113,25 +115,25 @@ editor no
 END
 
 mkdir -p /boot/loader/entries/
-touch /boot/loader/entries/arch.conf
 tee -a /boot/loader/entries/arch.conf << END
 title Arch Linux
 linux /vmlinuz-linux
 initrd /$cpu_microcode.img
 initrd /initramfs-linux.img
 options rd.luks.name=$(blkid -s UUID -o value /dev/nvme0n1p2)=cryptlvm root=/dev/vg0/root resume=/dev/vg0/swap rd.luks.options=discard$kernel_options nmi_watchdog=0 quiet rw
+lsm=landlock,lockdown,yama,apparmor,bpf
 END
 
-touch /boot/loader/entries/arch-lts.conf
 tee -a /boot/loader/entries/arch-lts.conf << END
 title Arch Linux LTS
 linux /vmlinuz-linux-lts
 initrd /$cpu_microcode.img
 initrd /initramfs-linux-lts.img
 options rd.luks.name=$(blkid -s UUID -o value /dev/nvme0n1p2)=cryptlvm root=/dev/vg0/root resume=/dev/vg0/swap rd.luks.options=discard$kernel_options nmi_watchdog=0 quiet rw
+lsm=landlock,lockdown,yama,apparmor,bpf
 END
 
-echo "Setting up Pacman hook for automatic systemd-boot updates"
+# Setup Pacman hook for automatic systemd-boot updates
 mkdir -p /etc/pacman.d/hooks/
 touch /etc/pacman.d/hooks/systemd-boot.hook
 tee -a /etc/pacman.d/hooks/systemd-boot.hook << END
@@ -146,17 +148,20 @@ When = PostTransaction
 Exec = /usr/bin/bootctl update
 END
 
-echo "Setting swappiness to 20"
+# Set swappiness to 20
 touch /etc/sysctl.d/99-swappiness.conf
 echo 'vm.swappiness=20' > /etc/sysctl.d/99-swappiness.conf
 
-echo "Enabling periodic TRIM"
+# Enable periodic TRIM
 systemctl enable fstrim.timer
 
-echo "Enabling NetworkManager"
-systemctl enable NetworkManager
+# Enable NetworkManager service
+systemctl enable NetworkManager.service
 
-echo "Adding user as a sudoer"
+# Enable Apparmor service
+systemctl enable apparmor.service
+
+# Add user as a sudoer
 echo '%wheel ALL=(ALL) ALL' | EDITOR='tee -a' visudo
 EOF
 
