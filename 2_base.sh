00feb125a3f703b4556a5f0a07d449316bf8fc32
gjpin <3874515+gjpin@users.noreply.github.com>
2021-12-30 18:25:41 +0000

    bashrc: prevent variable expansion

--- 2_base.sh
+++ 2_base.sh
@@ -31,21 +31,19 @@ mkdir -p ${HOME}/src
 
 # bashrc configurations
 tee -a ${HOME}/.bashrc << EOF
+
 # User specific environment
-if ! [[ "$PATH" =~ "$HOME/.local/bin:$HOME/bin:" ]]
+if ! [[ "\$PATH" =~ "\$HOME/.local/bin:\$HOME/bin:" ]]
 then
-    PATH="$HOME/.local/bin:$HOME/bin:$PATH"
+    PATH="\$HOME/.local/bin:\$HOME/bin:\$PATH"
 fi
 export PATH
-
-# Uncomment the following line if you don't like systemctl's auto-paging feature:
-# export SYSTEMD_PAGER=
 
 # User specific aliases and functions
 if [ -d ~/.bashrc.d ]; then
         for rc in ~/.bashrc.d/*; do
-                if [ -f "$rc" ]; then
-                        . "$rc"
+                if [ -f "\$rc" ]; then
+                        . "\$rc"
                 fi
         done
 fi
