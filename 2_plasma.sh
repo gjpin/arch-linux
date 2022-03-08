07ce004a7f0f13edb22b2c4f58edfd3a4d5efa1f
Guilherme <3874515+gjpin@users.noreply.github.com>
2022-03-08 12:27:51 +0000

    Update 2_plasma.sh

--- 2_plasma.sh
+++ 2_plasma.sh
@@ -23,15 +23,10 @@ sudo pacman -S --noconfirm kdeconnect sshfs
 
 # Install KeePassXC
 sudo flatpak install -y flathub org.keepassxc.KeePassXC
-sudo flatpak override --nofilesystem=host org.keepassxc.KeePassXC
-sudo flatpak override --nodevice=all org.keepassxc.KeePassXC
-sudo flatpak override --nosocket=x11 org.keepassxc.KeePassXC
 sudo flatpak override --unshare=network org.keepassxc.KeePassXC
-sudo flatpak override --filesystem=${HOME}/Sync/credentials org.keepassxc.KeePassXC
 
 # Install Breeze-GTK flatpak theme and allow Flatpaks to access GTK configs
 sudo flatpak install -y flathub org.gtk.Gtk3theme.Breeze
-sudo flatpak install -y flathub org.gtk.Gtk3theme.Breeze-dark
 sudo flatpak override --filesystem=xdg-config/gtk-3.0:ro
 sudo flatpak override --filesystem=xdg-config/gtk-4.0:ro
 
@@ -47,29 +42,10 @@ User=$USER
 Session=plasmawayland
 EOF
 
-# Customize bash
-tee -a ${HOME}/.bashrc.d/prompt << EOF
-PS1="\[\e[1;36m\]\w\[\e[m\] \[\e[1;33m\]\\$\[\e[m\] "
-PROMPT_COMMAND="export PROMPT_COMMAND=echo"
-EOF
-
-# Download wallpaper
-mkdir $HOME/Pictures/wallpapers/
-wget -P $HOME/Pictures/wallpapers/ https://raw.githubusercontent.com/gjpin/arch-linux/master/images/wallpapers/hans-isaacson-MC5tMO1V-vs-unsplash.jpg
-
-# Import konsole Github color schemes
-wget -P ${HOME}/.local/share/konsole https://raw.githubusercontent.com/gjpin/arch-linux/master/dotfiles/konsole/dark.colorscheme
-wget -P ${HOME}/.local/share/konsole https://raw.githubusercontent.com/gjpin/arch-linux/master/dotfiles/konsole/light.colorscheme
-
 # Configure Plasma
 kwriteconfig5 --file kdeglobals --group KDE --key LookAndFeelPackage "org.kde.breezetwilight.desktop"
 kwriteconfig5 --file kdeglobals --group KDE --key SingleClick --type bool true
 kwriteconfig5 --file kdeglobals --group KDE --key AnimationDurationFactor "0.5"
-kwriteconfig5 --file kdeglobals --group General --key Name "Breeze Light"
-
-# Enable OpenGL 3.1
-kwriteconfig5 --file kwinrc --group Compositing --key GLCore --type bool true
-kwriteconfig5 --file kwinrc --group Compositing --key OpenGLIsUnsafe --type bool false
 
 # Enable 2 desktops
 kwriteconfig5 --file kwinrc --group Desktops --key Name_2 "Desktop 2"
@@ -80,10 +56,6 @@ kwriteconfig5 --file kwinrc --group Desktops --key Rows "1"
 kwriteconfig5 --file kwinrc --group org.kde.kdecoration2 --key ButtonsOnLeft ""
 kwriteconfig5 --file kwinrc --group org.kde.kdecoration2 --key ButtonsOnRight "IAX"
 kwriteconfig5 --file kwinrc --group org.kde.kdecoration2 --key ShowToolTips --type bool false
-kwriteconfig5 --file breezerc --group Common --key OutlineCloseButton --type bool false
-
-# Disable baloo file indexer
-kwriteconfig5 --file baloofilerc --group "Basic Settings" --key Indexing-Enabled --type bool false
 
 # Configure Konsole
 kwriteconfig5 --file konsolerc --group KonsoleWindow --key SaveGeometryOnExit --type bool false
@@ -97,22 +69,104 @@ kwriteconfig5 --file kwinrc --group Effect-PresentWindows --key BorderActivateAl
 kwriteconfig5 --file kwinrc --group TabBox --key BorderActivate "9"
 
 # Change Task Switcher behaviour
-kwriteconfig5 --file kwinrc --group TabBox --key BorderActivate "9"
-kwriteconfig5 --file kwinrc --group TabBox --key HighlightWindows --type bool false
+kwriteconfig5 --file kwinrc --group TabBox --key HighlightWindows  --type bool false
 kwriteconfig5 --file kwinrc --group TabBox --key LayoutName "thumbnail_grid"
+
+# Use Scale window animation
+kwriteconfig5 --file kwinrc --group Plugins --key kwin4_effect_fadeEnabled --type bool false
+kwriteconfig5 --file kwinrc --group Plugins --key kwin4_effect_scaleEnabled --type bool true
 
 # Disable splash screen
 kwriteconfig5 --file ksplashrc --group KSplash --key Engine "none"
 kwriteconfig5 --file ksplashrc --group KSplash --key Theme "none"
 
-# Change date time formats
-#kwriteconfig5 --file plasma-org.kde.plasma.desktop-appletsrc --group Containments --group 4 --group Applets --group 21 --group Configuration --group Appearance --key customDateFormat "dd MM yyyy"
-#kwriteconfig5 --file plasma-org.kde.plasma.desktop-appletsrc --group Containments --group 4 --group Applets --group 21 --group Configuration --group Appearance --key dateFormat "custom"
-#kwriteconfig5 --file plasma-org.kde.plasma.desktop-appletsrc --group Containments --group 4 --group Applets --group 21 --group Configuration --group Appearance --key use24hFormat "2"
+# Import Konsole Github color schemes
+wget -P $HOME/.local/share/konsole https://raw.githubusercontent.com/gjpin/fedora-kde/main/konsole/dark.colorscheme
+wget -P $HOME/.local/share/konsole https://raw.githubusercontent.com/gjpin/fedora-kde/main/konsole/light.colorscheme
 
-# Set wallpaper
-kwriteconfig5 --file kscreenlockerrc --group Greeter --group Wallpaper --group org.kde.image --group General --key Image "$HOME/Pictures/wallpapers/hans-isaacson-MC5tMO1V-vs-unsplash.jpg"
-kwriteconfig5 --file plasmarc --group Wallpapers --key usersWallpapers "$HOME/Pictures/wallpapers/hans-isaacson-MC5tMO1V-vs-unsplash.jpg"
-kwriteconfig5 --file plasma-org.kde.plasma.desktop-appletsrc --group Containments --group 1 --group Wallpaper --group org.kde.image --group General --key Image "$HOME/Pictures/wallpapers/hans-isaacson-MC5tMO1V-vs-unsplash.jpg"
+# Customize bash
+tee -a ~/.bashrc.d/prompt << EOF
+PS1="\[\e[1;36m\]\w\[\e[m\] \[\e[1;33m\]\\$\[\e[m\] "
+PROMPT_COMMAND="export PROMPT_COMMAND=echo"
+EOF
 
+# Disable app launch feedback
+kwriteconfig5 --file klaunchrc --group BusyCursorSettings --key "Bouncing" --type bool false
+kwriteconfig5 --file klaunchrc --group FeedbackStyle --key "BusyCursor" --type bool false
+
+# Autologin
+sudo kwriteconfig5 --file /etc/sddm.conf.d/kde_settings.conf --group Autologin --key "Relogin" --type bool false
+sudo kwriteconfig5 --file /etc/sddm.conf.d/kde_settings.conf --group Autologin --key "Session" "plasma"
+sudo kwriteconfig5 --file /etc/sddm.conf.d/kde_settings.conf --group Autologin --key "User" "${USER}"
+
+# SDDM theme
+sudo kwriteconfig5 --file /etc/sddm.conf.d/kde_settings.conf --group Theme --key "Current" "breeze"
+
+# Enable overview
+sudo kwriteconfig5 --file kwinrc --group Plugins --key "overviewEnabled" --type bool true
+kwriteconfig5 --file kglobalshortcutsrc --group kwin --key "Overview" "Meta+Tab,none,Toggle Overview"
+
+# Use KDE Wallet to store ssh key passphrases
+mkdir -p ~/.config/autostart/
+tee -a ~/.config/autostart/ssh-add.desktop << EOF
+[Desktop Entry]
+Exec=ssh-add -q /home/$USER/.ssh/id_ed25519
+Name=ssh-add
+Type=Application
+EOF
+
+mkdir -p ~/.config/plasma-workspace/env/
+tee -a ~/.config/plasma-workspace/env/askpass.sh << EOF
+#!/bin/sh
+export SSH_ASKPASS='/usr/bin/ksshaskpass'
+EOF
+
+chmod +x ~/.config/plasma-workspace/env/askpass.sh
+
+#SSH_ASKPASS='/usr/bin/ksshaskpass'
+#ssh-add ~/.ssh/id_ed25519 </dev/null
+
+##### SHORTCUTS
+# Desktop switch
+kwriteconfig5 --file kglobalshortcutsrc --group plasmashell --key "activate task manager entry 1" "none,none,Activate Task Manager Entry 1"
+kwriteconfig5 --file kglobalshortcutsrc --group plasmashell --key "activate task manager entry 2" "none,none,Activate Task Manager Entry 2"
+kwriteconfig5 --file kglobalshortcutsrc --group plasmashell --key "activate task manager entry 3" "none,none,Activate Task Manager Entry 3"
+kwriteconfig5 --file kglobalshortcutsrc --group plasmashell --key "activate task manager entry 4" "none,none,Activate Task Manager Entry 4"
+kwriteconfig5 --file kglobalshortcutsrc --group plasmashell --key "activate task manager entry 5" "none,none,Activate Task Manager Entry 5"
+kwriteconfig5 --file kglobalshortcutsrc --group plasmashell --key "activate task manager entry 6" "none,none,Activate Task Manager Entry 6"
+kwriteconfig5 --file kglobalshortcutsrc --group plasmashell --key "activate task manager entry 7" "none,none,Activate Task Manager Entry 7"
+kwriteconfig5 --file kglobalshortcutsrc --group plasmashell --key "activate task manager entry 8" "none,none,Activate Task Manager Entry 8"
+kwriteconfig5 --file kglobalshortcutsrc --group plasmashell --key "activate task manager entry 9" "none,none,Activate Task Manager Entry 9"
+kwriteconfig5 --file kglobalshortcutsrc --group plasmashell --key "activate task manager entry 10" "none,none,Activate Task Manager Entry 10"
+
+kwriteconfig5 --file kglobalshortcutsrc --group kwin --key "Switch to Desktop 1" "Meta+1,none,Switch to Desktop 1"
+kwriteconfig5 --file kglobalshortcutsrc --group kwin --key "Switch to Desktop 2" "Meta+2,none,Switch to Desktop 2"
+kwriteconfig5 --file kglobalshortcutsrc --group kwin --key "Switch to Desktop 3" "Meta+3,none,Switch to Desktop 3"
+kwriteconfig5 --file kglobalshortcutsrc --group kwin --key "Switch to Desktop 4" "Meta+4,none,Switch to Desktop 4"
+kwriteconfig5 --file kglobalshortcutsrc --group kwin --key "Switch to Desktop 5" "Meta+5,none,Switch to Desktop 5"
+kwriteconfig5 --file kglobalshortcutsrc --group kwin --key "Switch to Desktop 6" "Meta+6,none,Switch to Desktop 6"
+kwriteconfig5 --file kglobalshortcutsrc --group kwin --key "Switch to Desktop 7" "Meta+7,none,Switch to Desktop 7"
+kwriteconfig5 --file kglobalshortcutsrc --group kwin --key "Switch to Desktop 8" "Meta+8,none,Switch to Desktop 8"
+kwriteconfig5 --file kglobalshortcutsrc --group kwin --key "Switch to Desktop 9" "Meta+9,none,Switch to Desktop 9"
+kwriteconfig5 --file kglobalshortcutsrc --group kwin --key "Switch to Desktop 10" "Meta+0,none,Switch to Desktop 10"
+
+kwriteconfig5 --file kglobalshortcutsrc --group kwin --key "Window to Desktop 1" "Meta+\!,none,Window to Desktop 1"
+kwriteconfig5 --file kglobalshortcutsrc --group kwin --key "Window to Desktop 2" "Meta+@,none,Window to Desktop 2"
+kwriteconfig5 --file kglobalshortcutsrc --group kwin --key "Window to Desktop 3" "Meta+#,none,Window to Desktop 3"
+kwriteconfig5 --file kglobalshortcutsrc --group kwin --key "Window to Desktop 4" "Meta+$,none,Window to Desktop 4"
+kwriteconfig5 --file kglobalshortcutsrc --group kwin --key "Window to Desktop 5" "Meta+%,none,Window to Desktop 5"
+kwriteconfig5 --file kglobalshortcutsrc --group kwin --key "Window to Desktop 6" "Meta+^,none,Window to Desktop 6"
+kwriteconfig5 --file kglobalshortcutsrc --group kwin --key "Window to Desktop 7" "Meta+&,none,Window to Desktop 7"
+kwriteconfig5 --file kglobalshortcutsrc --group kwin --key "Window to Desktop 8" "Meta+*,none,Window to Desktop 8"
+kwriteconfig5 --file kglobalshortcutsrc --group kwin --key "Window to Desktop 9" "Meta+(,none,Window to Desktop 9"
+kwriteconfig5 --file kglobalshortcutsrc --group kwin --key "Window to Desktop 10" "Meta+),none,Window to Desktop 10"
+
+# Konsole
+kwriteconfig5 --file kglobalshortcutsrc --group org.kde.konsole.desktop --key "_launch" "Meta+Return,none,Konsole"
+
+# Close windows
+kwriteconfig5 --file kglobalshortcutsrc --group kwin --key "Window Close" "Meta+Shift+Q,none,Close Window"
+
+# Spectacle
+kwriteconfig5 --file kglobalshortcutsrc --group "org.kde.spectacle.desktop" --key "RectangularRegionScreenShot" "Meta+Shift+S,none,Capture Rectangular Region"
 echo "Your setup is ready. You can reboot now!"
