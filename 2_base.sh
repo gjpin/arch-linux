e6e3d080d51b84e7a8b5e60d28b17986dcea37dc
Guilherme <3874515+gjpin@users.noreply.github.com>
2022-03-08 18:38:30 +0000

    Update 2_base.sh

--- 2_base.sh
+++ 2_base.sh
@@ -53,7 +53,7 @@ unset rc
 EOF
 
 tee -a ${HOME}/.bashrc.d/defaults << EOF
-export EDITOR=vim
+export EDITOR=neovim
 EOF
 
 # Install fonts
@@ -72,7 +72,7 @@ sudo pacman -S --noconfirm mesa $gpu_drivers vulkan-icd-loader
 sudo pacman -S --noconfirm ffmpeg libva-utils libva-vdpau-driver vdpauinfo gst-libav
 
 # Install common applications
-sudo pacman -S --noconfirm vim git openssh upower htop powertop p7zip ripgrep unzip fwupd unrar bash-completion
+sudo pacman -S --noconfirm neovim git openssh upower htop powertop p7zip ripgrep unzip fwupd unrar bash-completion
 
 # Add Flathub repositories
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
