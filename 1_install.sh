e42ad1e3dd187af52667654cb8c82f84cc9d7031
gjpin <3874515+gjpin@users.noreply.github.com>
2022-04-21 12:42:22 +0100

    wip

--- 1_install.sh
+++ 1_install.sh
@@ -1,32 +1,36 @@
 #!/bin/bash
 
-luks_password=""
-username=""
-user_password=""
-hostname=""
-timezone=""
+# Variables
+LUKS_PASSWORD=""
+USERNAME=""
+USER_PASSWORD=""
+HOSTNAME=""
+TIMEZONE=""
 
-read -p "LUKS password: " luks_password
-read -p "Username: " username
-read -p "User password: " user_password
-read -p "Hostname: " hostname
-read -p "Timezone (timedatectl list-timezones): " timezone
+read -p "LUKS password: " LUKS_PASSWORD
+read -p "Username: " USERNAME
+read -p "User password: " USER_PASSWORD
+read -p "Hostname: " HOSTNAME
+read -p "Timezone (timedatectl list-timezones): " TIMEZONE
 
-# Set different microcode, kernel params and initramfs modules according to CPU vendor
-cpu_vendor=$(cat /proc/cpuinfo | grep vendor | uniq)
-cpu_microcode=""
-kernel_options=""
-initramfs_modules=""
-if [[ $cpu_vendor =~ "AuthenticAMD" ]]
+CPU_VENDOR=$(cat /proc/cpuinfo | grep vendor | uniq)
+CPU_MICROCODE=""
+INITRAMFS_MODULES=""
+KERNEL_OPTIONS=""
+
+if [[ $CPU_VENDOR =~ "AuthenticAMD" ]]
 then
- cpu_microcode="amd-ucode"
- initramfs_modules="amdgpu"
-elif [[ $cpu_vendor =~ "GenuineIntel" ]]
+ CPU_MICROCODE="amd-ucode"
+ INITRAMFS_MODULES="amdgpu"
+elif [[ $CPU_VENDOR =~ "GenuineIntel" ]]
 then
- cpu_microcode="intel-ucode"
- kernel_options=" i915.enable_fbc=1"
- initramfs_modules="i915"
+ CPU_MICROCODE="intel-ucode"
+ INITRAMFS_MODULES="i915"
+ KERNEL_OPTIONS=" i915.enable_fbc=1"
 fi
+
+TOTAL_MEM=$(free -g | grep Mem: | awk '{print $2}')
+SWAP_SIZE=$(( $TOTAL_MEM + 1 ))
 
 # Update system clock
 timedatectl set-ntp true
@@ -34,28 +38,37 @@ timedatectl set-ntp true
 # Sync packages database
 pacman -Sy --noconfirm
 
-# Wipe drive
+# Zero out all GPT data structures
 sgdisk --zap-all /dev/nvme0n1
 
-# Create partition tables
-parted --script --align optimal /dev/nvme0n1 \
-    mkpart "EFI system partition" fat32 0% 512MiB \
-    set 1 esp on \
-    mkpart "root partition" ext4 512MiB 100%
+# Create partition tables (sgdisk -L)
+sgdisk -og /dev/nvme0n1
+sgdisk --new 1:4096:+512M --typecode 1:ef00 --change-name 1:"EFI System Partition" /dev/nvme0n1
+sgdisk --new 2:0:+"$SWAP_SIZE"G --typecode 2:8309 --change-name 2:"Swap Partition" /dev/nvme0n1
+ENDSECTOR=$(sgdisk -E /dev/nvme0n1)
+sgdisk --new 3:0:"$ENDSECTOR" --typecode 3:8309 --change-name 3:"Root Partition" /dev/nvme0n1
 
-# Eanble full system encryption with dm-crypt + LUKS (except /boot)
-echo "$luks_password" | cryptsetup luksFormat /dev/nvme0n1p2
-echo "$luks_password" | cryptsetup open /dev/nvme0n1p2 root
+# Prepare swap partition: encrypt swap partition and create swap filesystem
+echo "$LUKS_PASSWORD" | cryptsetup luksFormat /dev/nvme0n1p2
+echo "$LUKS_PASSWORD" | cryptsetup open /dev/nvme0n1p2 swap
+mkswap /dev/mapper/swap
+swapon /dev/mapper/swap
 
+# Prepare root partition: encrypt root partition and create ext4 filesystem
+echo "$LUKS_PASSWORD" | cryptsetup luksFormat /dev/nvme0n1p3
+echo "$LUKS_PASSWORD" | cryptsetup open /dev/nvme0n1p3 root
 mkfs.ext4 /dev/mapper/root
 mount /dev/mapper/root /mnt
 
+# Prepare boot partition: create fat32 filesystem
+mkfs.fat -F32 /dev/nvme0n1p1
 mount --mkdir /dev/nvme0n1p1 /mnt/boot
 
 # Install Arch Linux
-pacstrap /mnt base base-devel efibootmgr linux linux-headers linux-lts linux-lts-headers \
-linux-firmware device-mapper dosfstools e2fsprogs $cpu_microcode cryptsetup networkmanager \
-wget man-db man-pages nano diffutils flatpak lm_sensors apparmor
+pacstrap /mnt base linux linux-lts linux-firmware $CPU_MICROCODE
+
+# pacstrap /mnt device-mapper dosfstools e2fsprogs cryptsetup networkmanager \
+# wget man-db man-pages nano diffutils flatpak lm_sensors apparmor
 
 # Generate fstab
 genfstab -U /mnt >> /mnt/etc/fstab
@@ -64,7 +77,7 @@ genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt /bin/bash << EOF
 # Set system clock
 timedatectl set-ntp true
-ln -sf /usr/share/zoneinfo/"$timezone" /etc/localtime
+ln -sf /usr/share/zoneinfo/"$TIMEZONE" /etc/localtime
 hwclock --systohc
 
 # Set locales
@@ -76,15 +89,15 @@ locale-gen
 echo "KEYMAP=us" > /etc/vconsole.conf
 
 # Set hostname
-echo $hostname > /etc/hostname
+echo $HOSTNAME > /etc/hostname
 
 # Create new user
-useradd -m -G wheel -s /bin/bash $username
-echo -en "$user_password\n$user_password" | passwd $username
+useradd -m -G wheel -s /bin/bash $USERNAME
+echo -en "$USER_PASSWORD\n$USER_PASSWORD" | passwd $USERNAME
 
 # Generate initramfs
 sed -i 's/^HOOKS.*/HOOKS=(base systemd autodetect keyboard sd-vconsole modconf block sd-encrypt filesystems fsck)/' /etc/mkinitcpio.conf
-sed -i 's/^MODULES.*/MODULES=(ext4 $initramfs_modules)/' /etc/mkinitcpio.conf
+sed -i 's/^MODULES.*/MODULES=(ext4 $INITRAMFS_MODULES)/' /etc/mkinitcpio.conf
 mkinitcpio -P
 
 # Setup systemd-boot
@@ -102,17 +115,17 @@ mkdir -p /boot/loader/entries/
 tee -a /boot/loader/entries/arch.conf << END
 title Arch Linux
 linux /vmlinuz-linux
-initrd /$cpu_microcode.img
+initrd /$CPU_MICROCODE.img
 initrd /initramfs-linux.img
-options rd.luks.name=$(blkid -s UUID -o value /dev/nvme0n1p2)=root root=/dev/mapper/root rd.luks.options=discard$kernel_options nowatchdog lsm=landlock,lockdown,yama,apparmor,bpf quiet rw
+options rd.luks.name=$(blkid -s UUID -o value /dev/nvme0n1p3)=root root=/dev/mapper/root rd.luks.options=discard$KERNEL_OPTIONS nowatchdog lsm=landlock,lockdown,yama,apparmor,bpf quiet rw
 END
 
 tee -a /boot/loader/entries/arch-lts.conf << END
 title Arch Linux LTS
 linux /vmlinuz-linux-lts
-initrd /$cpu_microcode.img
+initrd /$CPU_MICROCODE.img
 initrd /initramfs-linux-lts.img
-options rd.luks.name=$(blkid -s UUID -o value /dev/nvme0n1p2)=root root=/dev/mapper/root rd.luks.options=discard$kernel_options nowatchdog lsm=landlock,lockdown,yama,apparmor,bpf quiet rw
+options rd.luks.name=$(blkid -s UUID -o value /dev/nvme0n1p3)=root root=/dev/mapper/root rd.luks.options=discard$KERNEL_OPTIONS nowatchdog lsm=landlock,lockdown,yama,apparmor,bpf quiet rw
 END
 
 # Setup Pacman hook for automatic systemd-boot updates
