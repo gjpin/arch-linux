5bcff47d39af009d22bae799219eff3be005a0b3
gjpin <3874515+gjpin@users.noreply.github.com>
2023-08-11 22:07:49 +0100

    firefox: use betterfox user.js

--- extra/firefox.js
+++ extra/firefox.js
@@ -1,11 +1,264 @@
-// KDE integration
-// https://wiki.archlinux.org/title/firefox#KDE_integration
-user_pref("widget.use-xdg-desktop-portal.mime-handler", 1);
-user_pref("widget.use-xdg-desktop-portal.file-picker", 1);
-// user_pref("media.hardwaremediakeys.enabled", false);
+//
+/* You may copy+paste this file and use it as it is.
+ *
+ * If you make changes to your about:config while the program is running, the
+ * changes will be overwritten by the user.js when the application restarts.
+ *
+ * To make lasting changes to preferences, you will have to edit the user.js.
+ */
 
-// Set homepage
-user_pref("browser.startup.homepage", "chrome://browser/content/blanktab.html");
+/****************************************************************************
+ * Betterfox                                                                *
+ * "Ad meliora"                                                             *
+ * version: 115                                                             *
+ * url: https://github.com/yokoffing/Betterfox                              *
+****************************************************************************/
+
+/****************************************************************************
+ * SECTION: FASTFOX                                                         *
+****************************************************************************/
+user_pref("nglayout.initialpaint.delay", 0);
+user_pref("nglayout.initialpaint.delay_in_oopif", 0);
+user_pref("content.notify.interval", 100000);
+user_pref("browser.startup.preXulSkeletonUI", false);
+
+/** EXPERIMENTAL ***/
+user_pref("layout.css.grid-template-masonry-value.enabled", true);
+user_pref("dom.enable_web_task_scheduling", true);
+
+/** GFX ***/
+user_pref("gfx.webrender.all", true);
+user_pref("gfx.webrender.precache-shaders", true);
+user_pref("gfx.webrender.compositor", true);
+user_pref("layers.gpu-process.enabled", true);
+user_pref("media.hardware-video-decoding.enabled", true);
+user_pref("gfx.canvas.accelerated", true);
+user_pref("gfx.canvas.accelerated.cache-items", 32768);
+user_pref("gfx.canvas.accelerated.cache-size", 4096);
+user_pref("gfx.content.skia-font-cache-size", 80);
+user_pref("image.cache.size", 10485760);
+user_pref("image.mem.decode_bytes_at_a_time", 131072);
+user_pref("image.mem.shared.unmap.min_expiration_ms", 120000);
+user_pref("media.memory_cache_max_size", 1048576);
+user_pref("media.memory_caches_combined_limit_kb", 2560000);
+user_pref("media.cache_readahead_limit", 9000);
+user_pref("media.cache_resume_threshold", 6000);
+
+/** BROWSER CACHE ***/
+user_pref("browser.cache.memory.max_entry_size", 153600);
+
+/** NETWORK ***/
+user_pref("network.buffer.cache.size", 262144);
+user_pref("network.buffer.cache.count", 128);
+user_pref("network.http.max-connections", 1800);
+user_pref("network.http.max-persistent-connections-per-server", 10);
+user_pref("network.ssl_tokens_cache_capacity", 32768);
+
+/****************************************************************************
+ * SECTION: SECUREFOX                                                       *
+****************************************************************************/
+/** TRACKING PROTECTION ***/
+user_pref("browser.contentblocking.category", "strict");
+user_pref("urlclassifier.trackingSkipURLs", "*.reddit.com, *.twitter.com, *.twimg.com, *.tiktok.com");
+user_pref("urlclassifier.features.socialtracking.skipURLs", "*.instagram.com, *.twitter.com, *.twimg.com");
+user_pref("privacy.query_stripping.strip_list", "__hsfp __hssc __hstc __s _hsenc _openstat dclid fbclid gbraid gclid hsCtaTracking igshid mc_eid ml_subscriber ml_subscriber_hash msclkid oft_c oft_ck oft_d oft_id oft_ids oft_k oft_lk oft_sk oly_anon_id oly_enc_id rb_clickid s_cid twclid vero_conv vero_id wbraid wickedid yclid");
+user_pref("browser.uitour.enabled", false);
+user_pref("privacy.globalprivacycontrol.enabled", true);
+user_pref("privacy.globalprivacycontrol.functionality.enabled", true);
+
+/** OCSP & CERTS / HPKP ***/
+user_pref("security.OCSP.enabled", 0);
+user_pref("security.remote_settings.crlite_filters.enabled", true);
+user_pref("security.pki.crlite_mode", 2);
+user_pref("security.cert_pinning.enforcement_level", 2);
+
+/** SSL / TLS ***/
+user_pref("security.ssl.treat_unsafe_negotiation_as_broken", true);
+user_pref("browser.xul.error_pages.expert_bad_cert", true);
+user_pref("security.tls.enable_0rtt_data", false);
+
+/** DISK AVOIDANCE ***/
+user_pref("browser.cache.disk.enable", false);
+user_pref("browser.privatebrowsing.forceMediaMemoryCache", true);
+user_pref("browser.sessionstore.privacy_level", 2);
+
+/** SHUTDOWN & SANITIZING ***/
+user_pref("privacy.history.custom", true);
+
+/** SPECULATIVE CONNECTIONS ***/
+user_pref("network.http.speculative-parallel-limit", 0);
+user_pref("network.dns.disablePrefetch", true);
+user_pref("browser.urlbar.speculativeConnect.enabled", false);
+user_pref("browser.places.speculativeConnect.enabled", false);
+user_pref("network.prefetch-next", false);
+user_pref("network.predictor.enabled", false);
+user_pref("network.predictor.enable-prefetch", false);
+
+/** SEARCH / URL BAR ***/
+user_pref("browser.search.separatePrivateDefault.ui.enabled", true);
+user_pref("browser.urlbar.update2.engineAliasRefresh", true);
+user_pref("browser.search.suggest.enabled", false);
+user_pref("browser.urlbar.suggest.quicksuggest.sponsored", false);
+user_pref("browser.urlbar.suggest.quicksuggest.nonsponsored", false);
+user_pref("security.insecure_connection_text.enabled", true);
+user_pref("security.insecure_connection_text.pbmode.enabled", true);
+user_pref("network.IDN_show_punycode", true);
+
+/** HTTPS-FIRST MODE ***/
+user_pref("dom.security.https_first", true);
+
+/** PROXY / SOCKS / IPv6 ***/
+user_pref("network.proxy.socks_remote_dns", true);
+user_pref("network.file.disable_unc_paths", true);
+user_pref("network.gio.supported-protocols", "");
+
+/** PASSWORDS AND AUTOFILL ***/
+user_pref("signon.formlessCapture.enabled", false);
+user_pref("signon.privateBrowsingCapture.enabled", false);
+user_pref("signon.autofillForms", false);
+user_pref("signon.rememberSignons", false);
+user_pref("editor.truncate_user_pastes", false);
+
+/** ADDRESS + CREDIT CARD MANAGER ***/
+user_pref("extensions.formautofill.addresses.enabled", false);
+user_pref("extensions.formautofill.creditCards.enabled", false);
+user_pref("extensions.formautofill.heuristics.enabled", false);
+user_pref("browser.formfill.enable", false);
+
+/** MIXED CONTENT + CROSS-SITE ***/
+user_pref("network.auth.subresource-http-auth-allow", 1);
+user_pref("pdfjs.enableScripting", false);
+user_pref("extensions.postDownloadThirdPartyPrompt", false);
+user_pref("permissions.delegation.enabled", false);
+
+/** HEADERS / REFERERS ***/
+user_pref("network.http.referer.XOriginTrimmingPolicy", 2);
+
+/** CONTAINERS ***/
+user_pref("privacy.userContext.ui.enabled", true);
+
+/** WEBRTC ***/
+user_pref("media.peerconnection.ice.proxy_only_if_behind_proxy", true);
+user_pref("media.peerconnection.ice.default_address_only", true);
+
+/** SAFE BROWSING ***/
+user_pref("browser.safebrowsing.downloads.remote.enabled", false);
+
+/** MOZILLA ***/
+user_pref("accessibility.force_disabled", 1);
+user_pref("identity.fxaccounts.enabled", false);
+user_pref("browser.tabs.firefox-view", false);
+user_pref("permissions.default.desktop-notification", 2);
+user_pref("permissions.default.geo", 2);
+user_pref("geo.provider.network.url", "https://location.services.mozilla.com/v1/geolocate?key=%MOZILLA_API_KEY%");
+user_pref("geo.provider.ms-windows-location", false); // WINDOWS
+user_pref("geo.provider.use_corelocation", false); // MAC
+user_pref("geo.provider.use_gpsd", false); // LINUX
+user_pref("geo.provider.use_geoclue", false); // LINUX
+user_pref("permissions.manager.defaultsUrl", "");
+user_pref("webchannel.allowObject.urlWhitelist", "");
+
+/** TELEMETRY ***/
+user_pref("toolkit.telemetry.unified", false);
+user_pref("toolkit.telemetry.enabled", false);
+user_pref("toolkit.telemetry.server", "data:,");
+user_pref("toolkit.telemetry.archive.enabled", false);
+user_pref("toolkit.telemetry.newProfilePing.enabled", false);
+user_pref("toolkit.telemetry.shutdownPingSender.enabled", false);
+user_pref("toolkit.telemetry.updatePing.enabled", false);
+user_pref("toolkit.telemetry.bhrPing.enabled", false);
+user_pref("toolkit.telemetry.firstShutdownPing.enabled", false);
+user_pref("toolkit.telemetry.coverage.opt-out", true);
+user_pref("toolkit.coverage.opt-out", true);
+user_pref("datareporting.healthreport.uploadEnabled", false);
+user_pref("datareporting.policy.dataSubmissionEnabled", false);
+user_pref("app.shield.optoutstudies.enabled", false);
+user_pref("browser.discovery.enabled", false);
+user_pref("breakpad.reportURL", "");
+user_pref("browser.tabs.crashReporting.sendReport", false);
+user_pref("browser.crashReports.unsubmittedCheck.autoSubmit2", false);
+user_pref("captivedetect.canonicalURL", "");
+user_pref("network.captive-portal-service.enabled", false);
+user_pref("network.connectivity-service.enabled", false);
+user_pref("default-browser-agent.enabled", false);
+user_pref("app.normandy.enabled", false);
+user_pref("app.normandy.api_url", "");
+user_pref("browser.ping-centre.telemetry", false);
+user_pref("browser.newtabpage.activity-stream.feeds.telemetry", false);
+user_pref("browser.newtabpage.activity-stream.telemetry", false);
+
+/****************************************************************************
+ * SECTION: PESKYFOX                                                        *
+****************************************************************************/
+/** MOZILLA UI ***/
+user_pref("layout.css.prefers-color-scheme.content-override", 2);
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("app.update.suppressPrompts", true);
+user_pref("browser.compactmode.show", true);
+user_pref("browser.privatebrowsing.vpnpromourl", "");
+user_pref("extensions.getAddons.showPane", false);
+user_pref("extensions.htmlaboutaddons.recommendations.enabled", false);
+user_pref("browser.shell.checkDefaultBrowser", false);
+user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons", false);
+user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features", false);
+user_pref("browser.preferences.moreFromMozilla", false);
+user_pref("browser.tabs.tabmanager.enabled", false);
+user_pref("browser.aboutwelcome.enabled", false);
+user_pref("findbar.highlightAll", true);
+user_pref("middlemouse.contentLoadURL", false);
+user_pref("browser.privatebrowsing.enable-new-indicator", false);
+
+/** FULLSCREEN ***/
+user_pref("full-screen-api.transition-duration.enter", "0 0");
+user_pref("full-screen-api.transition-duration.leave", "0 0");
+user_pref("full-screen-api.warning.delay", -1);
+user_pref("full-screen-api.warning.timeout", 0);
+
+/** URL BAR ***/
+user_pref("browser.urlbar.suggest.engines", false);
+user_pref("browser.urlbar.suggest.topsites", false);
+user_pref("browser.urlbar.suggest.calculator", true);
+user_pref("browser.urlbar.unitConversion.enabled", true);
+
+/** NEW TAB PAGE ***/
+user_pref("browser.newtabpage.activity-stream.feeds.topsites", false);
+user_pref("browser.newtabpage.activity-stream.feeds.section.topstories", false);
+
+/*** POCKET ***/
+user_pref("extensions.pocket.enabled", false);
+
+/** DOWNLOADS ***/
+user_pref("browser.download.useDownloadDir", false);
+user_pref("browser.download.alwaysOpenPanel", false);
+user_pref("browser.download.manager.addToRecentDocs", false);
+user_pref("browser.download.always_ask_before_handling_new_types", true);
+
+/** PDF ***/
+user_pref("browser.download.open_pdf_attachments_inline", true);
+
+/** TAB BEHAVIOR ***/
+user_pref("browser.tabs.loadBookmarksInTabs", true);
+user_pref("browser.bookmarks.openInTabClosesMenu", false);
+user_pref("layout.css.has-selector.enabled", true);
+user_pref("cookiebanners.service.mode", 2);
+user_pref("cookiebanners.service.mode.privateBrowsing", 2);
+
+/****************************************************************************
+ * SECTION: SMOOTHFOX                                                       *
+****************************************************************************/
+// visit https://github.com/yokoffing/Betterfox/blob/master/Smoothfox.js
+// Enter your scrolling prefs below this line:
+
+/****************************************************************************
+ * START: MY OVERRIDES                                                      *
+****************************************************************************/
+// Enter your personal prefs below this line:
+
+// Disable experiments
+user_pref("experiments.supported", false);
+user_pref("experiments.enabled", false);
+user_pref("experiments.manifest.uri", "");
+user_pref("network.allow-experiments", false);
 
 // Enable FFMPEG VA-API
 user_pref("media.ffmpeg.vaapi.enabled", true);
@@ -15,140 +268,12 @@ user_pref("media.hardware-video-decoding.force-enabled", true);
 user_pref("gfx.webrender.all", true);
 user_pref("media.av1.enabled", false);
 
-// Disable title bar
-user_pref("browser.tabs.inTitlebar", 1);
+// KDE integration
+// https://wiki.archlinux.org/title/firefox#KDE_integration
+user_pref("widget.use-xdg-desktop-portal.mime-handler", 1);
+user_pref("widget.use-xdg-desktop-portal.file-picker", 1);
+// user_pref("media.hardwaremediakeys.enabled", false);
 
-// Disable View feature
-user_pref("browser.tabs.firefox-view", false);
-
-// Disable List All Tabs button
-user_pref("browser.tabs.tabmanager.enabled", false);
-
-// Disable password manager
-user_pref("signon.rememberSignons", false);
-
-// Disable default browser check
-user_pref("browser.shell.checkDefaultBrowser", false);
-
-// Enable scrolling with middle mouse button
-user_pref("general.autoScroll", true);
-
-// Enable Firefox Tracking Protection
-user_pref("browser.contentblocking.category", "strict");
-user_pref("privacy.trackingprotection.enabled", true);
-user_pref("privacy.trackingprotection.pbmode.enabled", true);
-user_pref("privacy.trackingprotection.fingerprinting.enabled", true);
-user_pref("privacy.trackingprotection.cryptomining.enabled", true);
-user_pref("privacy.trackingprotection.socialtracking.enabled", true);
-user_pref("network.cookie.cookieBehavior", 5);
-
-// Disable Mozilla telemetry/experiments
-user_pref("toolkit.telemetry.enabled", false);
-user_pref("toolkit.telemetry.unified", false);
-user_pref("toolkit.telemetry.archive.enabled", false);
-user_pref("experiments.supported", false);
-user_pref("experiments.enabled", false);
-user_pref("experiments.manifest.uri", "");
-
-// Disallow Necko to do A/B testing
-user_pref("network.allow-experiments", false);
-
-// Disable collection/sending of the health report
-user_pref("datareporting.healthreport.uploadEnabled", false);
-user_pref("datareporting.healthreport.service.enabled", false);
-user_pref("datareporting.policy.dataSubmissionEnabled", false);
-user_pref("browser.discovery.enabled", false);
-
-// Disable Pocket
-user_pref("browser.pocket.enabled", false);
-user_pref("extensions.pocket.enabled", false);
-user_pref("browser.newtabpage.activity-stream.feeds.section.topstories", false);
-
-// Disable Location-Aware Browsing (geolocation)
-user_pref("geo.enabled", false);
-
-// Disable "beacon" asynchronous HTTP transfers (used for analytics)
-user_pref("beacon.enabled", false);
-
-// Disable speech recognition
-user_pref("media.webspeech.recognition.enable", false);
-
-// Disable speech synthesis
-user_pref("media.webspeech.synth.enabled", false);
-
-// Disable pinging URIs specified in HTML <a> ping= attributes
-user_pref("browser.send_pings", false);
-
-// Don't try to guess domain names when entering an invalid domain name in URL bar
-user_pref("browser.fixup.alternate.enabled", false);
-
-// Opt-out of add-on metadata updates
-user_pref("extensions.getAddons.cache.enabled", false);
-
-// Opt-out of themes (Persona) updates
-user_pref("lightweightThemes.update.enabled", false);
-
-// Disable Flash Player NPAPI plugin
-user_pref("plugin.state.flash", 0);
-
-// Disable Java NPAPI plugin
-user_pref("plugin.state.java", 0);
-
-// Disable Gnome Shell Integration NPAPI plugin
-user_pref("plugin.state.libgnome-shell-browser-plugin", 0);
-
-// Updates addons automatically
-user_pref("extensions.update.enabled", true);
-
-// Enable add-on and certificate blocklists (OneCRL) from Mozilla
-user_pref("extensions.blocklist.enabled", true);
-user_pref("services.blocklist.update_enabled", true);
-
-// Disable Extension recommendations
-user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr", false);
-
-// Disable sending Firefox crash reports to Mozilla servers
-user_pref("breakpad.reportURL", "");
-
-// Disable sending reports of tab crashes to Mozilla
-user_pref("browser.tabs.crashReporting.sendReport", false);
-user_pref("browser.crashReports.unsubmittedCheck.enabled", false);
-
-// Disable Shield/Heartbeat/Normandy
-user_pref("app.normandy.enabled", false);
-user_pref("app.normandy.api_url", "");
-user_pref("extensions.shield-recipe-client.enabled", false);
-user_pref("app.shield.optoutstudies.enabled", false);
-
-// Disable Firefox Hello metrics collection
-user_pref("loop.logDomains", false);
-
-// Enable blocking reported web forgeries
-user_pref("browser.safebrowsing.phishing.enabled", true);
-
-// Enable blocking reported attack sites
-user_pref("browser.safebrowsing.malware.enabled", true);
-
-// Disable downloading homepage snippets/messages from Mozilla
-user_pref("browser.aboutHomeSnippets.updateUrl", "");
-
-// Enable Content Security Policy (CSP)
-user_pref("security.csp.experimentalEnabled", true);
-
-// Enable Subresource Integrity
-user_pref("security.sri.enable", true);
-
-// Don't send referer headers when following links across different domains
-user_pref("network.http.referer.XOriginPolicy", 2);
-
-// Disable new tab tile ads & preload
-user_pref("browser.newtabpage.enhanced", false);
-user_pref("browser.newtab.preload", false);
-user_pref("browser.newtabpage.directory.ping", "");
-user_pref("browser.newtabpage.directory.source", "data:text/plain,{}");
-
-// Enable HTTPS-Only Mode
-user_pref("dom.security.https_only_mode", true);
-
-// Enable HSTS preload list
-user_pref("network.stricttransportsecurity.preloadlist", true);
+/****************************************************************************
+ * END: BETTERFOX                                                           *
+****************************************************************************/
